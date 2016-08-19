d50feba45ba041a0cb44bfbfba6999b849f3615e
Scott Baldwin <scott.baldwin@oracle.com>
2016-08-19 12:38:35 -0500

    Reverting text back to where it started

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "<h1>Hello world original from: $HOSTNAME</h1>" > /usr/share/nginx/html/index.html
+echo "<h1>Hello world from: $HOSTNAME</h1>" > /usr/share/nginx/html/index.html
 
 # Use `exec` so that PID 1 is the nginx process and not this script
 exec /usr/sbin/nginx -g "daemon off;"
