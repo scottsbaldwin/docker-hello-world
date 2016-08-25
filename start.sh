53998fa1ff60974a490e161933e8d03d0b2a8521
Scott Baldwin <scott.baldwin@oracle.com>
2016-08-25 15:01:10 -0500

    Changing HTML message for testing

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "<h1>Hello webhook world from: $HOSTNAME</h1>" > /usr/share/nginx/html/index.html
+echo "<h1>Hello blue/green world from: $HOSTNAME</h1>" > /usr/share/nginx/html/index.html
 
 # Use `exec` so that PID 1 is the nginx process and not this script
 exec /usr/sbin/nginx -g "daemon off;"
