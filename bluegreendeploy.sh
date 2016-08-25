548d5d1e3f8a456e36820defb30ac01854f1f8e4
Scott Baldwin <scott.baldwin@oracle.com>
2016-08-25 14:47:24 -0500

    change service name to app

--- bluegreendeploy.sh
+++ bluegreendeploy.sh
@@ -21,7 +21,7 @@ POSTDATA=$(cat <<ENDOFTEMPLATE
     "pool_id": "default"
   },
   "quantities": {
-    "helloworld": 3
+    "app": 3
   },
   "stack": {
     "content": "version: 2\nservices:\n  app:\n    image: \"${DOCKER_REGISTRY}/${IMAGE_NAME}:${TRAVIS_BUILD_NUMBER}\"\n    ports:\n      - ${EXPOSED_PORT}/tcp\n    environment:\n      - \"occs:availability=per-pool\"\n      - \"occs:scheduler=random\"\n",
