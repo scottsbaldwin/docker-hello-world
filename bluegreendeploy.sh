44de86c2f75243e1d670cbefd5d1cfbde732453b
Scott Baldwin <scott.baldwin@oracle.com>
2016-08-25 13:54:23 -0500

    Add flag for insecure curl

--- bluegreendeploy.sh
+++ bluegreendeploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -ex
 
+INSECURE_CURL="-k"
+
 # ------------------------------------
 # Step 1: Determine target color
 # ------------------------------------
-ORIGINAL_COLOR=$(curl -s -XGET -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/current?raw=true)
+ORIGINAL_COLOR=$(curl ${INSECURE_CURL} -s -XGET -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/current?raw=true)
 TARGET_COLOR=$(echo "bluegreen" | sed -e s/${ORIGINAL_COLOR}//)
 
 # ------------------------------------
@@ -31,12 +33,12 @@ POSTDATA=$(cat <<ENDOFTEMPLATE
 ENDOFTEMPLATE
 )
 
-curl -XPOST -H "Authorization: Bearer ${API_TOKEN}" -d "${POSTDATA}" ${SERVICE_MANAGER}/api/v2/deployments/
+curl ${INSECURE_CURL} -XPOST -H "Authorization: Bearer ${API_TOKEN}" -d "${POSTDATA}" ${SERVICE_MANAGER}/api/v2/deployments/
 
 # ------------------------------------
 # Step 3: Store service discovery key for target color
 # ------------------------------------
-curl -s -XPUT -H "Authorization: Bearer ${API_TOKEN}" -d "apps/app-${APP_NAME}-${TARGET_COLOR}-${EXPOSED_PORT}/containers" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/${TARGET_COLOR}/id
+curl ${INSECURE_CURL} -s -XPUT -H "Authorization: Bearer ${API_TOKEN}" -d "apps/app-${APP_NAME}-${TARGET_COLOR}-${EXPOSED_PORT}/containers" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/${TARGET_COLOR}/id
 
 # ------------------------------------
 # Step 4: Wait for the target color to come online
@@ -47,7 +49,7 @@ WAIT_SECONDS=5
 HEALTHY=0
 while [ $TRY -lt $MAX_TRIES ]; do
  TRY=$(( $TRY + 1 ))
- RESPONSE=$(curl -s -XGET -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${TARGET_COLOR} | jq ".deployment | .current_state == .desired_state")
+ RESPONSE=$(curl ${INSECURE_CURL} -s -XGET -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${TARGET_COLOR} | jq ".deployment | .current_state == .desired_state")
 
  if [ "RESPONSE" == "true" ]; then
   HEALTHY=1
@@ -67,13 +69,13 @@ fi
 # ------------------------------------
 # Step 5: Point load balancer at new services
 # ------------------------------------
-curl -s -XPUT -H "Authorization: Bearer ${API_TOKEN}" -d "${TARGET_COLOR}" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/current
+curl ${INSECURE_CURL} -s -XPUT -H "Authorization: Bearer ${API_TOKEN}" -d "${TARGET_COLOR}" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/current
 
 # ------------------------------------
 # Step 6: Remove original deployment
 # ------------------------------------
 # First, the deployment must be stopped
-curl -s -XPOST -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${ORIGINAL_COLOR}/stop
+curl ${INSECURE_CURL} -s -XPOST -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${ORIGINAL_COLOR}/stop
 
 # Make sure it has stopped
 TRY=0
@@ -82,7 +84,7 @@ WAIT_SECONDS=5
 STOPPED=0
 while [ $TRY -lt $MAX_TRIES ]; do
  TRY=$(( $TRY + 1 ))
- RESPONSE=$(curl -s -XGET -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${TARGET_COLOR} | jq ".deployment | .current_state == .desired_state")
+ RESPONSE=$(curl ${INSECURE_CURL} -s -XGET -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${TARGET_COLOR} | jq ".deployment | .current_state == .desired_state")
 
  if [ "RESPONSE" == "true" ]; then
   STOPPED=1
@@ -95,8 +97,8 @@ done
 if [ $STOPPED -gt 0 ]; then
   # Finally, remove the deployment, and reset the ID
   echo "Original deployment has stopped. Removing the deployment for ${APP_NAME}-${ORIGINAL_COLOR}."
-  curl -s -XDELETE -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${ORIGINAL_COLOR}
-  curl -s -XPUT -H "Authorization: Bearer ${API_TOKEN}" -d "blue-green/null" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/${ORIGINAL_COLOR}/id
+  curl ${INSECURE_CURL} -s -XDELETE -H "Authorization: Bearer ${API_TOKEN}" ${SERVICE_MANAGER}/api/v2/deployments/${APP_NAME}-${ORIGINAL_COLOR}
+  curl ${INSECURE_CURL} -s -XPUT -H "Authorization: Bearer ${API_TOKEN}" -d "blue-green/null" ${SERVICE_MANAGER}/api/kv/blue-green/${APP_NAME}/${ORIGINAL_COLOR}/id
 else
   echo "Checked ${MAX_TRIES} times but deployment is not stopped. You may need to stop it manually."
 fi
